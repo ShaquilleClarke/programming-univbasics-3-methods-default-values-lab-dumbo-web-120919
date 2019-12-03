133dbb6cfcf20a60a585289eb98a15ca17d544d3
Shaquille Clarke <shaqstaysfresh@gmail.com>
2019-12-03 19:50:41 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -6,5 +6,5 @@ def meal_choice(veg1, veg2, protein = 'meat')
   puts meal
 end
 
-meal_choice("broccoli", "macaroni", "")
+meal_choice("broccoli", "macaroni")
 meal_choice("broccoli", "macaroni", "tofu")
