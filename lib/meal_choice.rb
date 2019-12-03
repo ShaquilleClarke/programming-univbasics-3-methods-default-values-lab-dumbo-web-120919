fb43a5d197f6c59deb3240a4b85a95c1dd5c6025
Shaquille Clarke <shaqstaysfresh@gmail.com>
2019-12-03 19:40:05 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -5,4 +5,5 @@ def meal_choice(veg1, veg2, protein = 'meat')
   puts "A plate #{protein} with #{veg1} and #{veg2}"
 end
 
-meal_choice("broccoli", "macaroni", "meat")
+meal_choice("broccoli", "macaroni", "")
+meal_choice("broccoli", "macaroni", "tofu")
